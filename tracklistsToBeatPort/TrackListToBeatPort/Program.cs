ac0e77c353c564eef9c73a0514b523e381c1acbe
Jon Hallam <Jon@decisions.com>
2022-04-30 18:22:49 +0100

    done

---
+++ tracklistsToBeatPort/TrackListToBeatPort/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace TrackListToBeatPort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Paste in your 1001TrackLists url");
+            var url = Console.ReadLine();
+             //   "https://www.1001tracklists.com/tracklist/mwzxpm9/charlie-hedges-shermanology-bbc-radio-1-dance-anthems-2022-04-23.html";
+            var tracks = new TrackListsToBeatPortClass.Class1().GetXpath(url);
+            var html = new TrackListsToBeatPortClass.Class1().ReturnHTMLOfListsOfTracks(tracks, url);
+            var filename = Guid.NewGuid() + ".html";
+            System.IO.File.WriteAllText(filename, html);
+
+            try
+            {
+                Process.Start(filename);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("open this file " + System.Environment.CurrentDirectory  + Path.DirectorySeparatorChar + filename );
+            }
+        }
+    }
+}
---
+++ tracklistsToBeatPort/TrackListsToBeatPortClass/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Xml;
+using HtmlAgilityPack;
+using RestSharp;
+
+namespace TrackListsToBeatPortClass
+{
+    public class Class1
+    {
+        
+        public string[] GetXpath(string url)
+        {
+            List<string> LS = new List<string>();
+            HtmlAgilityPack.HtmlWeb web = new HtmlWeb();
+            HtmlAgilityPack.HtmlDocument doc = web.Load (url);
+
+            foreach (HtmlNode row in doc.DocumentNode.SelectNodes("//*[@itemprop=\"tracks\"]")) 
+            {
+                LS.Add(row.InnerText);     
+            }
+
+            return LS.ToArray();
+        }
+
+        public string ReturnHTMLOfListsOfTracks(string[] Tracks, string tracklistingName)
+        {
+            string top ="<!DOCTYPE html><html><body><h2>" +tracklistingName +"</h2>";
+            string bottom = "</body></html>";
+            List<string> LS = new List<string>();
+
+
+            string html = top;
+            html = html + "<ul>";
+            string link = "";
+            
+            foreach (var track in Tracks)
+            {
+                var cleantrack = StripHTML(track).Trim();
+                var cleantrackForSearch = StripHTML(track).Trim().Replace("&", "%26").Replace(" ", "+");
+                string linkinner = "<li><a href=\"https://www.beatport.com/search?q="  +cleantrackForSearch + "\"" + ">" + cleantrack + "</a></li>" + System.Environment.NewLine;
+                link = link + linkinner;
+                LS.Add(track);
+            }
+
+            html = html + link;
+            
+            html = html + "</ul>";
+
+            html = html + bottom;
+
+            return html;
+
+
+        }
+        
+        public static string StripHTML(string input)
+        {
+            return Regex.Replace(input, "<.*?>", String.Empty);
+        }
+        
+        
+        
+    }
+}
